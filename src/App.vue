af89ecb67bdda48a8bc17462ea2c27b4b9bedcb4
David Hui <opensource@davidhui.ca>
2020-11-21 19:45:26 -0500

    dark mode o_0

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <div class="site-wrapper">
+    <div :class="'site-wrapper' + getPageStateClass()">
       <div class="splash-wrapper">
-        <div class="vmcs-logo-wrapper">
+        <div class="vmcs-logo-wrapper" @click="toggleColorMode">
           <img class="img-fluid vmcs-logo-main" alt="Vincent Massey Computer Science Club logo" src="./assets/vmcs-logo-vector.svg">
         </div>
 
         <h3>Vincent Massey Computer Science Club</h3>
-        <p>We are a group of students from Vincent Massey Secondary School who are passionate about computer science.</p>
+        <p>A group of students from Vincent Massey Secondary School who are passionate about computer science.</p>
         <p class="text-center social-link-collection">
-          <a class="social-link" href="https://party.vmcs.club"><font-awesome-icon icon="gavel"></font-awesome-icon> DOMJudge</a>
-          <a class="social-link" href="#"><font-awesome-icon icon="cubes"></font-awesome-icon> 7 Week Challenge</a>
+          <a :class="'social-link' + getPageStateClass()" href="https://party.vmcs.club"><font-awesome-icon icon="gavel"></font-awesome-icon> DOMJudge</a>
+          <a :class="'social-link' + getPageStateClass()" href="#"><font-awesome-icon icon="cubes"></font-awesome-icon> 7 Week Challenge</a>
         </p>
       </div>
     </div>
 
     <div class="footer">
       <p class="text-center social-link-collection">
-        <a class="social-link" href="https://watch.vmcs.club"><font-awesome-icon :icon="['fab', 'youtube']"></font-awesome-icon></a>
-        <a class="social-link" href="https://github.com/VMCSC"><font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon></a>
-        <a class="social-link" href="mailto:hello@vmcs.club"><font-awesome-icon icon="envelope"></font-awesome-icon></a>
+        <a :class="'social-link' + getPageStateClass()" href="https://watch.vmcs.club"><font-awesome-icon :icon="['fab', 'youtube']"></font-awesome-icon></a>
+        <a :class="'social-link' + getPageStateClass()" href="https://github.com/VMCSC"><font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon></a>
+        <a :class="'social-link' + getPageStateClass()" href="mailto:hello@vmcs.club"><font-awesome-icon icon="envelope"></font-awesome-icon></a>
       </p>
     </div>
   </div>
@@ -28,6 +28,19 @@
 <script>
 export default {
   name: 'App',
+  data (){
+    return {
+      darkMode: false
+    }
+  },
+  methods: {
+    toggleColorMode(){
+      this.darkMode = !this.darkMode;
+    },
+    getPageStateClass() {
+      return this.darkMode ? ' dark-mode' : ''
+    }
+  }
 }
 </script>
 
