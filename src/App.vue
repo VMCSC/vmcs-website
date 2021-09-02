6d1d6e3f30dbdac7222235e3552a6acf4febfde4
Gordon Lin <contact@gordonlin.ca>
2021-09-02 11:55:34 -0400

    Updated website

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,7 @@
         <h3>Vincent Massey Computer Science Club</h3>
         <p>A group of students from Vincent Massey Secondary School dedicated to creating opportunities for students to learn more about computer science and technology.</p>
         <p class="text-center social-link-collection">
-          <a :class="'social-link' + getPageStateClass()" href="https://party.vmcs.club"><font-awesome-icon icon="gavel"></font-awesome-icon> DOMJudge</a>
-          <a :class="'social-link' + getPageStateClass()" href="https://7wc.vmcs.club"><font-awesome-icon icon="cubes"></font-awesome-icon> 7 Week Challenge</a>
-          <a :class="'social-link' + getPageStateClass()" href="https://medium.com/vmcsc"><font-awesome-icon :icon="['fab', 'medium']"></font-awesome-icon> Blog</a>
+          <!--a :class="'social-link' + getPageStateClass()" href="https://party.vmcs.club"><font-awesome-icon icon="gavel"></font-awesome-icon> DOMJudge</a-->
         </p>
       </div>
     </div>
@@ -20,7 +18,6 @@
       <p class="text-center social-link-collection">
         <a :class="'social-link' + getPageStateClass()" href="https://watch.vmcs.club"><font-awesome-icon :icon="['fab', 'youtube']"></font-awesome-icon></a>
         <a :class="'social-link' + getPageStateClass()" href="https://github.com/VMCSC"><font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon></a>
-        <a :class="'social-link' + getPageStateClass()" href="https://medium.com/vmcsc"><font-awesome-icon :icon="['fab', 'medium']"></font-awesome-icon></a>
         <a :class="'social-link' + getPageStateClass()" href="mailto:hello@vmcs.club"><font-awesome-icon icon="envelope"></font-awesome-icon></a>
       </p>
     </div>
